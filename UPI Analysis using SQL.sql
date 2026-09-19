f81211e087f8587a01e0d487ded60b447ce8f33c
Reenashri02 <reenashri02@gmail.com>
2026-09-19 16:27:57 +0530

    Add files via upload

---
+++ UPI Analysis using SQL.sql
@@ -0,0 +1,12 @@
+Business Insights:
+
+1.Transaction Volume – Helps understand the overall usage of UPI transactions.
+2.Transaction Amount – Shows the total and average amount spent through UPI.
+3.Transaction Success Rate – Helps measure the performance of UPI payments.
+4.Transaction Type – Identifies the most commonly used type of UPI transaction.
+5.Merchant Category – Shows which merchant categories generate higher transaction amounts.
+6.State-wise Transactions – Identifies states with higher UPI transaction activity.
+7.Bank Usage – Helps identify the frequently used sender and receiver banks.
+8.Device Usage – Shows which devices customers commonly use for UPI payments.
+9.Customer Behavior – Age group and transaction-time analysis helps understand customer payment patterns.
+10.Fraud Analysis – Identifies fraudulent transactions and measures the overall fraud rate.
